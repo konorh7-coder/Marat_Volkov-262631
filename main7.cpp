887fbb0e6819d9087fa7588ac0b92633a53d2ee2
konorh7-coder <konorh7@gmail.com>
2026-06-15 17:46:31 +0200

    Update main7.cpp

--- main7.cpp
+++ main7.cpp
@@ -28,33 +28,6 @@ int main() {
     return(0);
 }
 
-
-
-
-//
-//#include <iostream>
-//#include <time.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main() {
-//    int lower, upper, quantity;
-//    int *tab;
-//    printf("Input the lower range of the array:");
-//    scanf("%i", &lower);
-//    printf("Input the upper range of the array:");
-//    scanf("%i", &upper);
-//    printf("Input how many numbers you want to generate:");
-//    scanf("%i", &quantity);
-//
-//    srand(time(NULL));
-//    tab = (int*) malloc(quantity*sizeof(int));
-//
-//    for (int i = 0; i < quantity; i++) {
-//        tab[i] = (rand() % (upper - lower + 1)) + lower;
-//        printf("array[%i] = %i\n", i, tab[i]);
-//    }
-//
 //    free(tab);
 //    return(0);
 //}
